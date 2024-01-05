922266dd05569326cca831628a5a83e3833333e3
Riley DuBord <thelifeofdu@gmail.com>
2024-01-05 15:21:21 -0600

    recording input values into JS variables

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 console.log('JS Works!');
 
 onReady();
-function onReady() {
 
+let employeeArray = [];
+
+function onReady() {
+    const formElement = document.getElementById("form");
+    formElement.addEventListener("submit", getFormData);
+    // let submitButton = document.getElementById("submit");
+  }
+
+function getFormData(event){
+    event.preventDefault();
+    let firstNameElement = event.target.children[0].value
+    let lastNameElement = event.target.children[1].value
+    let employeeIDElement = event.target.children[2].value
+    let employeeTitleElement = event.target.children[3].value
+    let employeeSalaryElement = event.target.children[4].value
+    
 }
